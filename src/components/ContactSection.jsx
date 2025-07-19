bc4b54f548a44bbe79c339fdfe73e3fb442781ec
vaibhav Chahar <149105747+vaibhavchahar@users.noreply.github.com>
2025-07-19 13:01:59 +0530

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -93,7 +93,7 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/vaibhavchahar " target="_blank">
                   <Linkedin />
                 </a>
                 <a href="#" target="_blank">
